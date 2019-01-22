7c746888a4a80b0aec5d67d250287f6abc17cbff
Jeffrey Chang <me@jeffchang.io>
2019-01-21 21:43:18 -0800

    Created method to pad lines with blank colored characters.

--- app/src/main/java/io/jeffchang/splitdiff/ui/gitdiff/widget/DiffTextView.kt
+++ app/src/main/java/io/jeffchang/splitdiff/ui/gitdiff/widget/DiffTextView.kt
@@ -13,6 +13,7 @@ import io.jeffchang.splitdiff.data.model.gitdiff.Content
 import io.jeffchang.splitdiff.ui.common.span
 
 /**
+ * Takes lines from a [Content] model and displays them on the screen in this view.
  */
 class DiffTextView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
@@ -29,6 +30,7 @@ class DiffTextView @JvmOverloads constructor(
 
     fun setDiffLines(diffType: DiffType, fromList: List<Content>) {
         val span = span {
+            // Goes through the list and create different spans, some with color, some without.
             fromList.forEach { content ->
                 if (content.type == Content.Type.SAME) {
                     +content.text.appendNewLineIfNeeded
@@ -37,7 +39,7 @@ class DiffTextView @JvmOverloads constructor(
                 diffType.color?.let {
                     val color = ContextCompat.getColor(context, it)
                     backgroundColor(color) {
-                        +(diffType.diffSymbol + content.text.appendNewLineIfNeeded)
+                        +(getColoredLine(diffType.diffSymbol, content.text))
                     }
                 }
             }
@@ -45,9 +47,23 @@ class DiffTextView @JvmOverloads constructor(
         text = span.build()
     }
 
+    // This takes all the parameters and creates correct colored lines in the text view.
+    private fun getColoredLine(diffSymbol: String, content: String): String {
+        // Finds how many single characters can find in the line.
+        val wordWidth = paint.measureText("a",0,1)
+        val screenWidth = context.resources.displayMetrics.widthPixels
+        val numberOfChars = (screenWidth / wordWidth).toInt()
+
+        // Pads all the lines with blank characters so it appears the entire line is colored.
+        val blanks = " ".repeat(numberOfChars - content.length)
+
+        return (diffSymbol + content + blanks).appendNewLineIfNeeded
+    }
+
     enum class DiffType(@ColorRes val color: Int?, val diffSymbol: String) {
         BEFORE(R.color.red, "-"),
         SAME(null, " "),
         AFTER(R.color.green, "+")
     }
+
 }
